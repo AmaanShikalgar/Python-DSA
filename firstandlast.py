3f6092c6fb1de7a1dee4bdea2d4fdbbbf9c6c781
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-06-26 15:32:24 +0530

    updated

--- firstandlast.py
+++ firstandlast.py
@@ -1,11 +1,10 @@
 def first_and_last(arr, target):
     first_index =-1
-    last_index=-1 
+    last_index=-1
     for i in range(len(arr)):
         if(arr[i]==target):
             if first_index == -1:
                 first_index = i
             last_index=i
     return first_index,last_index
-
 print(first_and_last(arr=[1,2,1,3,1],target=1))
