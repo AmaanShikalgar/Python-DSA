b7ad0fdb4f708459c5cd1e630ec57abdbef9f859
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-06-26 16:39:35 +0530

    updated

---
+++ binarySearch.py
@@ -0,0 +1,13 @@
+def binarySearch(arr,target):
+    left=0
+    right=len(arr)-1
+    while left<=right:
+        mid =(left+right)//2
+        if(arr[mid]==target):
+            return mid
+        elif(arr[mid]>target):
+            right=mid-1
+        else:
+            left=mid+1
+    return -1
+print(binarySearch([2,5,8,12,16,23,38,56,72],38))
