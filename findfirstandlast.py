3ab0ff05617c7ef26ca4ae9a9d48ab0be67e9fae
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-06-27 10:59:52 +0530

    updated

--- findfirstandlast.py
+++ findfirstandlast.py
@@ -3,7 +3,7 @@ def searchRange(nums,target):
     right=len(nums)-1
     while left<right:
         mid=(left+right)//2
-        if(nums[mid]>=target):
+        if nums[mid]>=target:
             right=mid
         else:
             left=mid+1
